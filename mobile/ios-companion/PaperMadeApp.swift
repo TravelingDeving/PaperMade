097afc7bc07d4f6eeb58804a372bc70f19f9f52b
TravelingTrader <devingtrader@proton.me>
2026-09-17 23:24:42 -0400

    Add shared PaperMade mobile app store

--- mobile/ios-companion/PaperMadeApp.swift
+++ mobile/ios-companion/PaperMadeApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct PaperMadeApp: App {
+    @StateObject private var store = PaperMadeStore()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(store)
                 .preferredColorScheme(.dark)
         }
     }
