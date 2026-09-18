7f177cf26d16bd4fbb0455b6e967f4df9a40fb36
TravelingTrader <devingtrader@proton.me>
2026-09-17 23:41:39 -0400

    Refresh shared PaperMade data when iOS app becomes active

--- mobile/ios-companion/PaperMadeApp.swift
+++ mobile/ios-companion/PaperMadeApp.swift
@@ -3,12 +3,18 @@ import SwiftUI
 @main
 struct PaperMadeApp: App {
     @StateObject private var store = PaperMadeStore()
+    @Environment(\.scenePhase) private var scenePhase
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
                 .preferredColorScheme(.dark)
+                .onChange(of: scenePhase) { _, phase in
+                    if phase == .active {
+                        store.reloadSharedSnapshots()
+                    }
+                }
         }
     }
 }
